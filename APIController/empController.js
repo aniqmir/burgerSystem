6d66820cb7489627173740acd9104b1f42ff0d79
zaki abbasi chotu lapi <zakiabbasi15@yahoo.com>
2018-05-22 05:21:08 +0500

    employee can  login

--- APIController/empController.js
+++ APIController/empController.js
@@ -1,7 +1,6 @@
 var express= require('express');
 var app= express();
-var jwt    = require('jsonwebtoken');
-
+var bcrypt = require('bcrypt-nodejs');
 //Middleware to connect to database
 var mongoose = require('mongoose');
 var config= require('../config');
@@ -13,26 +12,25 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 (db.on('error', console.error.bind(console, 'MongoDB connection error:')));
 
-//Function To Login
+
+var emp_instance =require('../models/emp');
+
+
 exports.loginandGetToken = function(req, res)
  {
-
-var nam= req.body.name;
-var pass= req.body.password;
-if(nam != 'nerd')
-{
-    return res.send({msg:'invalid Username'});
-}
-else if(pass != "1234")
-{
-   return res.send({msg:'password Invalid'});
-}
-else
-{
-   // res.send('login Successfull and token generated');
+    emp_instance.findOne(
+        // query
+        {Email:req.body.email}, (err, Emp) => {
+if (err) return res.status(200).json(err);
+if(Emp==null){return res.status(200).json(message='Invalid username');}
+bcrypt.compare(req.body.password, Emp.Emp_password, function(err, isMatch) {
+    if (err) { return res.json(err); }
+    // Password did not match
+    if (!isMatch) { return res.json({msg:'invalid Password'}) }
+    else {
     //Generate JWT Token
     const payload = {
-        name: nam
+        name: req.body.email
       };
           var token = jwt.sign(payload, config.secret, {expiresIn: 86400 // expires in 24 hours
         });
@@ -40,9 +38,11 @@ else
  //          return the information including token as JSON
         return res.json({
             success: true,
-            message: 'Enjoy your token!',
+            message: 'logged in!!! Enjoy your token!',
             token: token,
-            type: 'admin'
+            type: 'Employee'
           });
 }
+        });
+    });
 };
