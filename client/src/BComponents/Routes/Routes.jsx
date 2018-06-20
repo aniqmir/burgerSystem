6d454b2f572caa0e0990d8ac734ba8ec8b21ecd2
aniqmir <aniqmir@gmail.com>
2018-06-20 12:38:46 +0500

    folders adjusted

--- client/src/BComponents/Routes/Routes.jsx
+++ client/src/BComponents/Routes/Routes.jsx
@@ -7,20 +7,12 @@ import {
     }   from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
-<<<<<<< HEAD:client/src/BComponents/Routes/Routes.jsx
 import Navbar from '../Navbar/Navbar';
 import MainPage from '../MainPage/MainPage';
 import LandingPage from '../LandingPage/LandingPage'
 import Cart from '../Cart/Cart';
-=======
-import Navbar from './Navbar';
-import MainPage from './MainPage';
-import Features from './Features';
-import LandingPage from './LandingPage'
-import Cart from './Cart';
-import CheckOut from './CheckOut';
-import Cms from '../cms/admin/login';
->>>>>>> 225e493499d777c3fb53304a15312ff657d05456:client/src/BComponents/Routes.jsx
+import CheckOut from '../Cart/CheckOut';
+
 
 const customHistory = createBrowserHistory();
 
@@ -31,12 +23,7 @@ const CustomRoutes = () => (
             <Route exact path='/' component={LandingPage}/>
             <Route exact path='/cart' component={Cart}/>
             <Route path='/home' component={MainPage}/>
-<<<<<<< HEAD:client/src/BComponents/Routes/Routes.jsx
-=======
-            <Route path='/f' component={Features}/>
             <Route path='/checkout'  component={CheckOut}/>
-            <Route path='/cms'  component={Cms}/>
->>>>>>> 225e493499d777c3fb53304a15312ff657d05456:client/src/BComponents/Routes.jsx
         </div>
     </Router>
     )
