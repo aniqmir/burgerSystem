9cb6f685587b8442ec286af9f6f239bf00270c10
Faizan <mirzafaizanejaz@gmail.com>
2018-07-04 15:17:36 +0500

    User can now see total in checkout

--- client/src/endusers/Cart/CheckoutTable.jsx
+++ client/src/endusers/Cart/CheckoutTable.jsx
@@ -27,48 +27,65 @@ const styles = theme => ({
 
 class CustomizedTable extends React.Component {
     state={
-        data:[]
+        data:[],
+        total:0
     }
 
     componentWillMount() {
         let tempSessionCart = JSON.parse(localStorage.getItem('cartItems'));
+        let tempTotal = parseInt(localStorage.getItem('total'),10);
         console.log(tempSessionCart)
         this.setState({
-            data:tempSessionCart
+            data:tempSessionCart,
+            total:tempTotal
         });
+
+
     }
     render() {
-        const { classes } = this.props;
+      const { classes } = this.props;
  
-  return (
-    <Paper className={classes.root}>
-      <Table >
-        <TableHead>
-          <TableRow>
-            <CustomTableCell>Item Name</CustomTableCell>
-            <CustomTableCell>Price</CustomTableCell>
-            <CustomTableCell >Quantity</CustomTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.values(this.state.data).map((type,index) => {
-            return (
-              <TableRow key={index}>
-                <CustomTableCell >
-                  {type[0]}
-                </CustomTableCell>
-                <CustomTableCell >
-                  {type[1]}
-                </CustomTableCell>
-                <CustomTableCell>{type[6]}</CustomTableCell>
+      return (
+        <Paper className={classes.root}>
+          <Table >
+            <TableHead>
+              <TableRow>
+                <CustomTableCell>Item Name</CustomTableCell>
+                <CustomTableCell>Price</CustomTableCell>
+                <CustomTableCell >Quantity</CustomTableCell>
               </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
-}
+            </TableHead>
+            <TableBody>
+              {Object.values(this.state.data).map((type,index) => {
+                return (
+                  <TableRow key={index}>
+                    <CustomTableCell >
+                      {type[0]}
+                    </CustomTableCell>
+                    <CustomTableCell >
+                      {type[1]}
+                    </CustomTableCell>
+                    <CustomTableCell>{type[6]}</CustomTableCell>
+                  </TableRow>
+                );
+              })}
+              
+              <TableRow>
+                    <CustomTableCell >
+                     
+                    </CustomTableCell>
+                    <CustomTableCell >
+                      Total
+                    </CustomTableCell>
+                    <CustomTableCell>
+                      {this.state.total}
+                    </CustomTableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Paper>
+      );
+    }
 }
   
 
