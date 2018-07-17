808a393e90b43e28f1b8d5dc4292222327e83695
Faizan <mirzafaizanejaz@gmail.com>
2018-07-17 16:34:04 +0500

    Added step section

--- client/src/endusers/LandingPage/LandingPage.jsx
+++ client/src/endusers/LandingPage/LandingPage.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { Carousel, CarouselCaption, CarouselControl, CarouselInner, CarouselItem, CarouselIndicators, CarouselIndicator, View, Mask } from 'mdbreact';
 import './landingPage.css';
-
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class CarouselPage extends Component {
   constructor(props) {
@@ -41,9 +47,10 @@ class CarouselPage extends Component {
   }
 
   render(){
-    const { activeItem } = this.state;
     return(
-      <div className="d-block h-25 w-100" >
+      <div>
+
+        <div className="bg">
         <Carousel 
           activeItem={this.state.activeItem}
           next={this.next}
@@ -85,21 +92,75 @@ class CarouselPage extends Component {
                 <img className="d-block w-100 bg" src="http://www.craftlakewood.com/images/slideshow/craft-slide.jpg" alt="Mattonit's item" />
                 <Mask overlay="black-light"></Mask>
               </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Sopot Beach</h3>
-                <p>Taken june 21th by @mattonit</p>
-              </CarouselCaption>
             </CarouselItem>
           </CarouselInner>
-          <CarouselControl direction="prev" role="button" onClick={() => { this.prev(); }} />
-          <CarouselControl direction="next" role="button" onClick={() => { this.next(); }} />
-          <CarouselIndicators>
-            <CarouselIndicator active={activeItem === 1 ? true : false} onClick={() => { this.goToIndex(1); }}></CarouselIndicator>
-            <CarouselIndicator active={activeItem === 2 ? true : false} onClick={() => { this.goToIndex(2); }}></CarouselIndicator>
-            <CarouselIndicator active={activeItem === 3 ? true : false} onClick={() => { this.goToIndex(3); }}></CarouselIndicator>
-            <CarouselIndicator active={activeItem === 4 ? true : false} onClick={() => { this.goToIndex(4); }}></CarouselIndicator>
-          </CarouselIndicators>
-        </Carousel>
+        </Carousel>        
+        </div>
+
+
+        <div className="row">
+          <div className="container">
+        {/* title para */}
+            <div className="row pt-5">
+              <div className="col-sm-12">
+                <h1 className="text-center">
+                  Delivery Service
+                </h1>
+                <p className="pt-3">
+                Beat the Monday blues with the promise of a freshly-prepared GAIL’s breakfast at your morning meeting.
+                By the time you’re on your commute, the team at our bakeries in Seymour Place and Soho have already been up for hours kneading perfectly-formed mini loaves for our sharing sandwich platters and shaping delicate mini pastries, ready for delivery by courier to local businesses and corporate head offices in the area.
+                Whether it’s for a breakfast meeting or a working lunch, GAIL’s covers all your catering needs with a variety of wholesome options from healthy vegan and gluten-free salads to platters of mini sandwiches, sweets and savouries.
+                Simply fill your basket and order online by midday for next-day delivery from as early as 7:30am.
+
+                </p>
+              </div>
+            </div>
+
+        {/* Steps section */}
+            <div className="row pt-5">
+                 
+              <div className="col-sm-4">
+                <div className="box text-center">
+                  <h5 className="maron">
+                    Step 1
+                  </h5>
+                  <h3>
+                    BROWSE OUR MENU
+                  </h3>
+                  <small>
+                  Our mini sandwiches, savouries and sweet bites, as well as sharing salads are perfect for meetings and events.
+                  </small>
+                </div>
+              </div>  
+              <div className="col-sm-4">
+                <div className="box text-center">
+                  <h5 className="maron">
+                    Step 2
+                  </h5>
+                  <h3>
+                    ADD ITEMS TO CART
+                  </h3>
+                  <small>
+                    Select items from our menu &amp; add them to cart to continue.
+                  </small>
+                </div>
+              </div>  
+              <div className="col-sm-4">
+                <div className="box text-center">
+                  <h5 className="maron">
+                    Step 3
+                  </h5>
+                  <h3>
+                    FILL DELIVERY DETAILS
+                  </h3>
+                  <small>
+                    Fill your delivery details and click on checkout for delivery.
+                  </small>
+                </div>
+              </div>  
+            </div>
+          </div>
+        </div>
         </div>
     );
   }
